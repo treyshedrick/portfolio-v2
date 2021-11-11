cc155970c365855a9aac2f4c4cbc7d8a95a9dff7
treyshedrick <treyshedrick@gmail.com>
2021-11-10 21:10:31 -0600

    feat: Icons and links

--- src/components/app/constants.js
+++ src/components/app/constants.js
@@ -1,3 +1,9 @@
 export const SCREEN_SIZE = {
     SMALL: "(max-width: 700px)"
 };
+
+export const LINKS = {
+    LINKEDIN: "https://www.linkedin.com/in/treyshedrick",
+    GITHUB: "https://github.com/treyshedrick",
+    EMAIL: "mailto:treyshedrick@gmail.com"
+};
