1cb78a323a68f1bd7e531de8524c1196fc60de8d
treyshedrick <treyshedrick@gmail.com>
2021-11-11 09:17:44 -0600

    feat: contact anchors

---
+++ src/components/shared/ContactButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import { LABELS } from "../app/constants";
+
+const StyledAnchor = styled.a`
+    display: flex;
+    justify-content: baseline;
+    padding: 0px 5px;
+    text-decoration: none;
+    color: ${props => props.theme.inversePrimary};
+    &:hover {
+        opacity: .5;
+        color: ${props => props.theme.primary};
+    }
+`;
+
+const Text = styled.div`
+    font-size: ${props => props.theme.fontSize.md};
+    padding-top: ${props => props.customAlign ? "1px" : ""};
+    margin-left: 5px;
+`;
+
+const ContactButton = ({icon, text, src }) => {
+    const customAlign = text === LABELS.LINKEDIN;
+
+    return (
+        <StyledAnchor href={src} target={text !== LABELS.EMAIL && "_blank"}>
+            {icon}
+            <Text customAlign={customAlign}>{text}</Text>
+        </StyledAnchor>
+    );
+
+};
+
+export default ContactButton;
---
+++ src/components/shared/Icons.js
@@ -0,0 +1,5 @@
+import { Linkedin, GitHub, Mail } from 'react-feather';
+
+export const LinkedInIcon = () => <Linkedin size={20}/>;
+export const GitHubIcon = () => <GitHub size={20} />;
+export const MailIcon = () => <Mail size={20} />;
