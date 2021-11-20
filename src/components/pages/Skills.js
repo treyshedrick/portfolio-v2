5aece3fad632ff9046f66d4c16043cfa6abe6859
treyshedrick <treyshedrick@gmail.com>
2021-11-19 18:22:43 -0600

    feat: icon resize and skills page headers

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,42 +1,85 @@
 import React from "react";
 import Page from "../shared/Page";
+import styled from "styled-components";
 import * as Devicon from "../shared/Icons";
 import OrchestrationContainer from "../shared/OrchestrationContainer";
+
+const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+`;
+
+const Header = styled.div`
+    font-size: ${props => props.theme.fontSize.lg};
+    margin: ${props => props.isSmall ? "0px": "10px"};
+    border-bottom: 1px solid white;
+`;
+
+const SkillSetBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 30px 0px;
+`;
+
+const HeaderBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 40px 10px;
+`;
 
 const Skills = () => {
     return (
         <Page>
-            Front-End Development
-            <OrchestrationContainer>
-                <Devicon.ReactIcon />
-                <Devicon.HTMLIcon />
-                <Devicon.CSSIcon />
-                <Devicon.JavascriptIcon />
-                <Devicon.JQueryIcon />
-                <Devicon.BootStrapIcon />
-                <Devicon.MUIIcon />
-                <Devicon.TailwindIcon />
-            </OrchestrationContainer>
-            Back-End Development
-            <OrchestrationContainer>
-                <Devicon.NodeIcon />
-                <Devicon.ExpressIcon />
-                <Devicon.PHPIcon />
-                <Devicon.PostgresIcon />
-                <Devicon.MSSQLIcon />
-                <Devicon.MYSQLIcon />
-            </OrchestrationContainer>
-            Tools & Technologies
-            <OrchestrationContainer>
-                <Devicon.GitHubDIcon />
-                <Devicon.GitLabIcon />
-                <Devicon.AWSIcon />
-                <Devicon.JestIcon />
-                <Devicon.WebpackIcon />
-                <Devicon.LinuxIcon />
-                <Devicon.JiraIcon />
-                <Devicon.VSCodeIcon />
-            </OrchestrationContainer>
+            <HeaderBlock>
+                <Header>Skills</Header>
+                Everything from styling with styled components to creating relational databases, I create functional code that works and write tests using tools such as Jest and React Testing Library!
+            </HeaderBlock>
+            <SkillSetBlock>
+                <Header>Front-End Development</Header>
+                <OrchestrationContainer>
+                    <Row>
+                        <Devicon.ReactIcon />
+                        <Devicon.HTMLIcon />
+                        <Devicon.CSSIcon />
+                        <Devicon.JavascriptIcon />
+                        <Devicon.JQueryIcon />
+                        <Devicon.BootStrapIcon />
+                        <Devicon.MUIIcon />
+                        <Devicon.TailwindIcon />
+                    </Row>
+                </OrchestrationContainer>
+            </SkillSetBlock>
+            <SkillSetBlock>
+                <Header>Back-End Development</Header>
+                <OrchestrationContainer>
+                    <Row>
+                        <Devicon.NodeIcon />
+                        <Devicon.ExpressIcon />
+                        <Devicon.PHPIcon />
+                        <Devicon.PostgresIcon />
+                        <Devicon.MSSQLIcon />
+                        <Devicon.MYSQLIcon />
+                    </Row>
+                </OrchestrationContainer>
+            </SkillSetBlock>
+            <SkillSetBlock>
+                <Header>Tools & Technologies</Header>
+                <OrchestrationContainer>
+                    <Row>
+                        <Devicon.GitHubDIcon />
+                        <Devicon.GitLabIcon />
+                        <Devicon.AWSIcon />
+                        <Devicon.JestIcon />
+                        <Devicon.WebpackIcon />
+                        <Devicon.JiraIcon />
+                        <Devicon.VSCodeIcon />
+                    </Row>
+                </OrchestrationContainer>
+            </SkillSetBlock>
         </Page>
     );
 };
--- src/components/shared/Icons.js
+++ src/components/shared/Icons.js
@@ -23,7 +23,6 @@ import GitHubDevicon from 'react-devicons/github/original-wordmark';
 import GitLabDevicon from 'react-devicons/gitlab/original-wordmark';
 import AWSDevicon from 'react-devicons/amazonwebservices/original-wordmark';
 import JestDevicon from 'react-devicons/jest/plain';
-import LinuxDevicon from 'react-devicons/linux/plain';
 import WebpackDevicon from 'react-devicons/webpack/plain-wordmark';
 import JiraDevicon from 'react-devicons/jira/plain-wordmark';
 import VSCodeDevicon from 'react-devicons/vscode/original-wordmark';
@@ -46,29 +45,28 @@ export const MailIcon = () => <Mail size={20} />;
 // Devicon Icons
 
 // frontend
-export const ReactIcon = () => <StyledIcon variants={item} ><ReactDevicon size={80} /></StyledIcon>;
-export const HTMLIcon = () => <StyledIcon variants={item} ><Html5Devicon size={80} /></StyledIcon>;
-export const CSSIcon = () => <StyledIcon variants={item} ><CSS3Devicon size={80} /></StyledIcon>;
-export const JavascriptIcon = () => <StyledIcon variants={item} ><JavascriptDevicon size={80} /></StyledIcon>;
-export const JQueryIcon = () => <StyledIcon variants={item} ><JQueryDevicon size={80} /></StyledIcon>;
-export const BootStrapIcon = () => <StyledIcon variants={item} ><BootStrapDevicon size={80} /></StyledIcon>;
-export const MUIIcon = () => <StyledIcon variants={item} ><MUIDevicon size={80} /></StyledIcon>;
-export const TailwindIcon = () => <StyledIcon variants={item} ><TailWindDevicon size={80} /></StyledIcon>;
+export const ReactIcon = () => <StyledIcon variants={item} ><ReactDevicon size={75} /></StyledIcon>;
+export const HTMLIcon = () => <StyledIcon variants={item} ><Html5Devicon size={75} /></StyledIcon>;
+export const CSSIcon = () => <StyledIcon variants={item} ><CSS3Devicon size={75} /></StyledIcon>;
+export const JavascriptIcon = () => <StyledIcon variants={item} ><JavascriptDevicon size={75} /></StyledIcon>;
+export const JQueryIcon = () => <StyledIcon variants={item} ><JQueryDevicon size={75} /></StyledIcon>;
+export const BootStrapIcon = () => <StyledIcon variants={item} ><BootStrapDevicon size={75} /></StyledIcon>;
+export const MUIIcon = () => <StyledIcon variants={item} ><MUIDevicon size={75} /></StyledIcon>;
+export const TailwindIcon = () => <StyledIcon variants={item} ><TailWindDevicon size={75} /></StyledIcon>;
 
 // backend
-export const NodeIcon = () => <StyledIcon variants={item} ><NodeDevicon size={80} /></StyledIcon>;
-export const ExpressIcon = () => <StyledIcon variants={item} ><ExpressDevicon size={80} /></StyledIcon>;
-export const PHPIcon = () => <StyledIcon variants={item} ><PHPDevicon size={80} /></StyledIcon>;
-export const PostgresIcon = () => <StyledIcon variants={item} ><PostgresDevicon size={80} /></StyledIcon>;
-export const MSSQLIcon = () => <StyledIcon variants={item} ><MSSQLDevicon size={80} /></StyledIcon>;
-export const MYSQLIcon = () => <StyledIcon variants={item} ><MYSQLDevicon size={80} /></StyledIcon>;
+export const NodeIcon = () => <StyledIcon variants={item} ><NodeDevicon size={75} /></StyledIcon>;
+export const ExpressIcon = () => <StyledIcon variants={item} ><ExpressDevicon size={75} /></StyledIcon>;
+export const PHPIcon = () => <StyledIcon variants={item} ><PHPDevicon size={75} /></StyledIcon>;
+export const PostgresIcon = () => <StyledIcon variants={item} ><PostgresDevicon size={75} /></StyledIcon>;
+export const MSSQLIcon = () => <StyledIcon variants={item} ><MSSQLDevicon size={75} /></StyledIcon>;
+export const MYSQLIcon = () => <StyledIcon variants={item} ><MYSQLDevicon size={75} /></StyledIcon>;
 
 // tools / technologiesexport const ReactIcon = () => <StyledIcon variants={item} ><ReactDevicon size={80} /></StyledIcon>;
-export const GitHubDIcon = () => <StyledIcon variants={item} ><GitHubDevicon size={80} /></StyledIcon>;
-export const GitLabIcon = () => <StyledIcon variants={item} ><GitLabDevicon size={80} /></StyledIcon>;
-export const AWSIcon = () => <StyledIcon variants={item} ><AWSDevicon size={80} /></StyledIcon>;
-export const JestIcon = () => <StyledIcon variants={item} ><JestDevicon size={80} /></StyledIcon>;
-export const LinuxIcon = () => <StyledIcon variants={item} ><LinuxDevicon size={80} /></StyledIcon>;
-export const WebpackIcon = () => <StyledIcon variants={item} ><WebpackDevicon size={80} /></StyledIcon>;
-export const JiraIcon = () => <StyledIcon variants={item} ><JiraDevicon size={80} /></StyledIcon>;
-export const VSCodeIcon = () => <StyledIcon variants={item} ><VSCodeDevicon size={80} /></StyledIcon>;
+export const GitHubDIcon = () => <StyledIcon variants={item} ><GitHubDevicon size={75} /></StyledIcon>;
+export const GitLabIcon = () => <StyledIcon variants={item} ><GitLabDevicon size={75} /></StyledIcon>;
+export const AWSIcon = () => <StyledIcon variants={item} ><AWSDevicon size={75} /></StyledIcon>;
+export const JestIcon = () => <StyledIcon variants={item} ><JestDevicon size={75} /></StyledIcon>;
+export const WebpackIcon = () => <StyledIcon variants={item} ><WebpackDevicon size={75} /></StyledIcon>;
+export const JiraIcon = () => <StyledIcon variants={item} ><JiraDevicon size={75} /></StyledIcon>;
+export const VSCodeIcon = () => <StyledIcon variants={item} ><VSCodeDevicon size={75} /></StyledIcon>;
