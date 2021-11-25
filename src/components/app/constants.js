53ee79da3aeda74599aaada6bec1689ad093f0cb
treyshedrick <treyshedrick@gmail.com>
2021-11-24 18:35:40 -0600

    feat: project desription constants

--- src/components/app/constants.js
+++ src/components/app/constants.js
@@ -19,3 +19,10 @@ export const LABELS = {
     GITHUB: "GitHub",
     EMAIL: "Email"
 };
+
+export const PROJECT_DESCRIPTIONS = {
+    NINTENLIFE: "Mobile application for Nintendo enthusiasts that shows news and allows users to post anything Nintendo related!",
+    RN_QUICKSTART: "A quick and simple way to get started with react-native for both mobile and web. Need a quick proof of concept? This should help!",
+    BASEUI_ESLINT: "This plugin enforces users to spread the Base UI overrides prop into the component, so you can focus on the logic!",
+    COLOR_GAME: "Game made from Javascript that randomly generates a color to guess. Utilizes AJAX, PHP, and MariaDB to insert scores into a database. Also displays the high scores."
+};
