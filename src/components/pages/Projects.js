c2a5ab1df012ee33e3e8b1878a85ddb841672aa5
treyshedrick <treyshedrick@gmail.com>
2021-11-24 18:02:41 -0600

    feat: ProjectCard initial styling and ochestration

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,46 @@
 import React from "react";
 import Page from "../shared/Page";
+import styled from "styled-components";
+import OrchestrationContainer from "../shared/OrchestrationContainer";
+import ProjectCard from "../shared/ProjectCard";
+import ColorGameImage from "../../assets/color-game.png";
+
+
+const Header = styled.div`
+    font-size: ${props => props.theme.fontSize.lg};
+    margin: ${props => props.isSmall ? "0px": "10px"};
+    border-bottom: ${props => `1.5px solid ${props.theme.fontColor}`};
+`;
+
+const HeaderBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 40px 10px;
+`;
+
+const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
 
 const Projects = () => {
     return (
         <Page>
-            Projects
+            <HeaderBlock>
+                <Header>Projects</Header>
+                Check out some of the projects I have worked on!
+            </HeaderBlock>
+            <OrchestrationContainer>
+                <Row>
+                    <ProjectCard image={ColorGameImage} name="Color Game" />
+                    <ProjectCard image={ColorGameImage} />
+                    <ProjectCard image={ColorGameImage} />
+                    <ProjectCard image={ColorGameImage} />
+                </Row>
+            </OrchestrationContainer>
         </Page>
     );
 };
--- src/components/shared/ProjectCard.js
+++ src/components/shared/ProjectCard.js
@@ -3,22 +3,46 @@ import styled from "styled-components";
 import OrchestrationContainer from "./OrchestrationContainer";
 import { motion } from "framer-motion";
 
+const cardItem = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, y: 0},
+};
 
 const Card = styled(motion.div)`
     height: 400px;
-    width: 400px;
+    width: 375px;
+    margin: 25px 20px;
+    border: ${props => `1.5px solid ${props.theme.fontColor}`};
+    border-radius: 5px;
 `;
 
-const ProjectImage = styled.img`
+const ProjectImage = styled(motion.img)`
     width: 100%;
-    height: 50%;
+    height: 275px;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+    border-bottom: ${props => `1.5px solid ${props.theme.fontColor}`};
 `;
 
-const ProjectCard = ({image}) => {
+const ProjectTitle = styled.div`
+    font-size: ${props => props.theme.fontSize.lg};
+    margin-top: 5px;
+    text-align: center;
+`;
+
+const ProjectDescription = styled.div`
+    font-size: ${props => props.theme.fontSize.md};
+    margin-top: 5px;
+    text-align: center;
+`;
+
+const ProjectCard = ({image, name, description}) => {
     return(
-        <Card>
+        <Card variants={cardItem}>
             <OrchestrationContainer>
                 <ProjectImage src={image} />
+                <ProjectTitle>{name}</ProjectTitle>
+                <ProjectDescription>{description}</ProjectDescription>
             </OrchestrationContainer>
         </Card>
     );
